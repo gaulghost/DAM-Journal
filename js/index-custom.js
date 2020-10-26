7e5a179f45f57f824a4f390f8937a8438a3fc4e6
Pradhuman <49186033+gaulghost@users.noreply.github.com>
2020-10-26 21:33:51 +0530

    Add files via upload

--- js/index-custom.js
+++ js/index-custom.js
@@ -1,24 +1,107 @@
-/*-------------------------------------------------------------------------------
-  Equating containers
--------------------------------------------------------------------------------*/
-// retriving height of designing block under services section
-var elmnt1 = document.getElementById("service-designing");
-var num1 = elmnt1.offsetHeight;
-console.log(num1)
-// retriving height of blogging block under services section
-var elmnt2 = document.getElementById("service-blogging");
-var num2 = elmnt2.offsetHeight;
-console.log(num2)
+// CUSTOM JS
 
-// retriving height of coding block under services section
-var elmnt3 = document.getElementById("service-coding");
-var num3 = elmnt2.offsetHeight;
-console.log(num3)
-// Checking which is bigger
-var maximum = Math.max(num1, num2, num3);
-console.log(maximum)
 
-// Setting height of divs to equal from service section
-elmnt1.style.height = `${maximum}px`
-elmnt2.style.height = `${maximum}px`
-elmnt3.style.height = `${maximum}px`
+  /*-------------------------------------------------------------------------------
+    PRE LOADER
+  -------------------------------------------------------------------------------*/
+
+  $(window).load("../index.html",function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets
+  });
+  $(window).load("index.html",function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets
+  });
+
+  $(window).load("../coding.html",function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets
+  });
+  $(window).load("coding.html",function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets
+  });
+
+  $(window).load("../designing.html",function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets
+  });
+  $(window).load("designing.html",function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets
+  });
+
+  $(window).load("../blogging.html",function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets
+  });
+  $(window).load("blogging.html",function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets
+  });
+
+  /* HTML document is loaded. DOM is ready.
+  -------------------------------------------*/
+
+  $(document).ready(function() {
+
+
+  /*-------------------------------------------------------------------------------
+    Navigation - Hide mobile menu after clicking on a link
+  -------------------------------------------------------------------------------*/
+
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+
+if($(window).width() < 768){
+    $(".fixed-top").addClass("bg-dark");
+} else {
+    $(".fixed-top").removeClass("bg-dark");
+    $(window).scroll(function() {
+    if ($(".navbar").offset().top > 200) {
+        $(".fixed-top").addClass("bg-dark");
+    } else {
+        $(".fixed-top").removeClass("bg-dark");
+    }
+  });
+}
+
+  /*-------------------------------------------------------------------------------
+    jQuery Parallax
+  -------------------------------------------------------------------------------*/
+
+    function initParallax() {
+    $('#home').parallax("100%", 0.1);
+    $('#about').parallax("100%", 0.3);
+    $('#service').parallax("100%", 0.2);
+    $('#experience').parallax("100%", 0.3);
+    $('#education').parallax("100%", 0.1);
+    $('#quotes').parallax("100%", 0.3);
+    $('#contact').parallax("100%", 0.1);
+    $('footer').parallax("100%", 0.2);
+
+  }
+  initParallax();
+
+
+
+  /*-------------------------------------------------------------------------------
+    smoothScroll js
+  -------------------------------------------------------------------------------*/
+
+    $(function() {
+        $('.custom-navbar a, #home a, #fp-nav a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+
+
+  /*-------------------------------------------------------------------------------
+    wow js - Animation js
+  -------------------------------------------------------------------------------*/
+
+  new WOW({ mobile: false }).init();
+
+
+  });
